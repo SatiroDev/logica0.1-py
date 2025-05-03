39e2f3c94c515cb0992758e11741be6675524837
Satiro07 <jardelsatiro57@gmail.com>
2025-05-03 12:01:07 -0300

    Update exer2.py

--- try except/exer2.py
+++ try except/exer2.py
@@ -1,9 +1,11 @@
-import datetime
+# verificação de data
+
+from datetime import datetime
 
 try:
     data = input('Digite uma data: (yyyy-mm-dd) ')
-    print(datetime.strptime(data, '%Y-%m-%d'))
-
+    datetime.strptime(data, '%Y-%m-%d')
+    print('data válida!')
 
 except ValueError:
     print('Data inválida!')
